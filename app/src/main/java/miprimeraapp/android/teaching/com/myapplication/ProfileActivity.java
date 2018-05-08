5cb8f436ec86e261d6a4d23b5cbfdcac33200605
Nando <fersamed89@gmail.com>
2018-05-08 10:54:06 +0200

    Login con Linears layouts

--- app/src/main/java/miprimeraapp/android/teaching/com/myapplication/ProfileActivity.java
+++ app/src/main/java/miprimeraapp/android/teaching/com/myapplication/ProfileActivity.java
@@ -3,18 +3,52 @@ package miprimeraapp.android.teaching.com.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+
+import java.net.URLEncoder;
 
 public class ProfileActivity extends AppCompatActivity {
+    //Asigno los atributos
+    private EditText usernameEditText;
+    private EditText EmailEditText;
+    private EditText passwordEditText;
+    private EditText ageEditText;
+    private RadioButton radiobuttonM;
+    private RadioButton radioButtonF;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile_activity);
-        EditText usernameEDitText = findViewById(R.id.username);
-        String usuario = usernameEDitText.getText().toString();
+        //obtengo el Id con findViewById
+        usernameEditText = findViewById(R.id.username);
+        EmailEditText = findViewById(R.id.email);
+        passwordEditText = findViewById(R.id.password);
+        ageEditText = findViewById((R.id.age));
+        radioButtonF = findViewById(R.id.genderf);
+        radiobuttonM = findViewById(R.id.genderm);
+
     }
+    public void onClick5(View view) {
+        //Imprimo los datos con un get.text en Logcat
+        Log.d("ProfileActivity", "Username:" + usernameEditText.getText());
+        Log.d("ProfileActivity", "Email : " + EmailEditText.getText());
+        Log.d("ProfileActivity", "Password : " + passwordEditText.getText());
+        Log.d("ProfileActivity", "Age : " + ageEditText.getText());
+        //Radio button
+        if (radiobuttonM.isChecked()) {
+            Log.d("ProfileActivity", "Gender male");
+        } else if(radioButtonF.isChecked()) {
+            Log.d("ProfileActivity", "Gender female");
+
+        }
+
+        }
+
+
     public void onClick(View view) {
         Intent intent = new Intent(this, MainActivity.class);
 
